7d8c2b197d013ea7ea421a779ad342986c9eda1d
anasjlab <anasjlab.official@gmail.com>
2026-05-19 21:01:26 +0100

    deuxieme commit Jenkins

--- hello.py
+++ hello.py
@@ -1,24 +1,12 @@
-# hello.py (version sans input)
-import sys
-
 print("=" * 40)
 print("Bienvenue dans mon premier job Jenkins !")
 print("=" * 40)
 
-# On utilise un argument ou une variable par défaut
-if len(sys.argv) > 1:
-    nom = sys.argv[1]
-else:
-    nom = "Étudiant Jenkins"
-
+nom = input("Quel est ton nom ? ")
 print(f"Bonjour {nom}, ton job Jenkins a réussi !")
 
-# Petit calcul
 a = 10
 b = 5
+
 print(f"{a} + {b} = {a + b}")
 print(f"{a} - {b} = {a - b}")
-
-# Simuler un test
-assert a + b == 15, "Le test a échoué !"
-print("✅ Tous les tests passent avec succès")
